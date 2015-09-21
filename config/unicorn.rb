d7a2a088371462783013f9832b92dc673a5f3947
graf_abolmasov <graf@abolmasov.pro>
2015-09-21 13:33:18 +0400

    use one sidekiq worker

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
 # config/unicorn.rb
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 timeout 30
 preload_app true
 
 before_fork do |server, worker|
-  @sidekiq_pid ||= spawn('bundle exec sidekiq -c 2')
+  @sidekiq_pid ||= spawn('bundle exec sidekiq -c 1')
 
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
