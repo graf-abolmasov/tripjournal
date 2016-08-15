aefe356b06f4c09d9dcea0215474a97dbf3e4495
Pavel Abolmasov <graf@abolmasov.pro>
2016-08-15 12:09:07 +0400

    specify precision for lat lon

--- db/migrate/20160810182947_change_point_type.rb
+++ db/migrate/20160810182947_change_point_type.rb
@@ -1,14 +1,14 @@
 class ChangePointType < ActiveRecord::Migration
   def up
-    add_column :points, :lat, :decimal
-    add_column :points, :lng, :decimal
+    add_column :points, :lat, :decimal, precision: 9, scale: 6
+    add_column :points, :lng, :decimal, precision: 9, scale: 6
     execute 'update points set lat = latlng[0], lng=latlng[1]'
     remove_column :points, :latlng
     change_column :points, :lat, :decimal, null: false
     change_column :points, :lng, :decimal, null: false
-    change_column :points, :speed, :decimal, default: 0.0
-    change_column :points, :hdop, :decimal, default: 0.0
-    change_column :points, :alt, :decimal, default: 0.0
+    change_column :points, :speed, :decimal, default: 0.0, precision: 9, scale: 6
+    change_column :points, :hdop, :decimal, default: 0.0, precision: 9, scale: 6
+    change_column :points, :alt, :decimal, default: 0.0, precision: 9, scale: 6
   end
 
   def down
