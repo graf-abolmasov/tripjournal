c59a1c0af157076ef29db1cdec8a6f53767aa45f
Pavel Abolmasov <graf@abolmasov.pro>
2016-10-09 00:04:34 +0400

    inverse order for int points

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,6 +6,6 @@ class Note < ApplicationRecord
   belongs_to :source, polymorphic: true
 
   scope :with_location, -> { where.not(lat: nil).where.not(lng: nil) }
-  scope :chronologically_ordered, -> { order(created_at: :asc) }
+  scope :inverse_chronologically_ordered, -> { order(created_at: :desc) }
 
 end
