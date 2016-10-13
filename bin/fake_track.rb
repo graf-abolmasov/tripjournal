e4987bf9d4ccb56ca0672967dfc6e506735cb5e9
Pavel Abolmasov <graf@abolmasov.pro>
2016-10-13 22:11:58 +0400

    improve fake track

--- bin/fake_track.rb
+++ bin/fake_track.rb
@@ -2,14 +2,20 @@
 require 'gpx'
 require 'net/http'
 require 'ruby-progressbar'
+require 'byebug'
+require_relative '../app/services/kml_file'
+require_relative '../app/services/gpx_file'
+require_relative '../app/services/tracks_factory'
 
-host = 'localhost:3000'
+host = 'https://tripjournal.abolmasov.pro'
 
-gpx = GPX::GPXFile.new(:gpx_file => 'track.gpx')
-points = gpx.tracks.first.points
-progress = ProgressBar.create(total: points.length, format: '%a %B %p%% %r points/sec')
-points.each do |point|
-  uri = URI("http://#{host}/track?lat=#{point.lat}&lng=#{point.lon}")
-  Net::HTTP.get(uri)
-  progress.increment
+tracks = TracksFactory.send(:read_tracks_from_file, ARGV[0])
+
+tracks.each do |track|
+  progress = ProgressBar.create(total: track.length, format: '%a %B %p%% %r points/sec')
+  track.each do |point|
+    uri = URI("#{host}/track?lat=#{point[:x]}&lng=#{point[:y]}")
+    Net::HTTP.get(uri)
+    progress.increment
+  end
 end
