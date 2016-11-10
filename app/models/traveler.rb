a374ed6f1dfba6e64f617b27321b20470dab6ddd
Pavel Abolmasov <graf@abolmasov.pro>
2016-11-10 22:03:46 +0400

    Fix destroying dependent object

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,6 +1,6 @@
 class Traveler < ApplicationRecord
 
-  has_many :int_points
+  has_many :int_points, dependent: :delete_all
 
   scope :with_instagram, -> { where.not(instagram_token: nil) }
 
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ApplicationRecord
 
   validates :name, :description, :length, presence: true
 
-  has_many :tracks
-  has_many :int_points
+  has_many :tracks, dependent: :destroy
+  has_many :int_points, dependent: :delete_all
 
 end
